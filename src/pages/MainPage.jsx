f5ef05a64d7eacce7344e9173279de17ac6bd5ce
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-11-15 23:57:08 +0500

    Fix MainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,19 +1,22 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Outlet } from "react-router-dom";
-import styled from "styled-components";
-import SHeader from "../components/Header/Header";
-import SContainer from "../components/Container.styled";
-import SGlobalWrapper from "../components/GlobalWrapper.styled";
+// import React, { useState, useEffect, useContext } from "react";
+// import { Outlet } from "react-router-dom";
+// import styled from "styled-components";
+// import SHeader from "../components/Header/Header";
+// import SContainer from "../components/Container.styled";
+// import SGlobalWrapper from "../components/GlobalWrapper.styled";
+import { Main } from "../components/Main/Main";
+
 
 const MainPage = () => {
     return (
-        <>
-            <SHeader />
-            <SGlobalWrapper>
-                <SContainer>Здесь будет код</SContainer>
-            </SGlobalWrapper>
-            {/* <Outlet /> */}
-        </>
+        <Main />
+        // <>
+        //     <SHeader />
+        //     <SGlobalWrapper>
+        //         <SContainer>Здесь будет код</SContainer>
+        //     </SGlobalWrapper>
+        //     {/* <Outlet /> */}
+        // </>
     );
 };
 
