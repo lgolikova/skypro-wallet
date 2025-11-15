090232e4d4b079ad74ea168b0b49d10cedc7ce54
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-11-15 23:55:20 +0500

    Add component Category

---
+++ src/components/Category/Category.styled.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+
+export const SWrapper = styled.div`
+  height: 31px;
+  width: 89px;
+  padding: 8px 20px;
+  background-color: #F4F5F6;
+  border-radius: 30px;
+`;
+
+export const SContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const SIcon = styled.svg`
+  height: 14px;
+  width: 14px;
+`;
+export const STitle = styled.p`
+  /* font-family: Montserrat; */
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 100%;
+  /* letter-spacing: 0%; */
+  text-align: center;
+  vertical-align: middle;
+
+`;
