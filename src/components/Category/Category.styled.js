f1a3d97d1ca95da4de084d965c9016a3c629001c
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-11-16 13:16:27 +0500

    Done component Category

--- src/components/Category/Category.styled.js
+++ src/components/Category/Category.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const SWrapper = styled.div`
   height: 31px;
-  width: 89px;
+  max-width: min-content;
   padding: 8px 20px;
   background-color: #F4F5F6;
   border-radius: 30px;
@@ -14,19 +14,18 @@ export const SContent = styled.div`
   justify-content: space-between;
   flex-direction: row;
   align-items: center;
+  gap: 12px;
 `;
 
-export const SIcon = styled.svg`
+export const SIcon = styled.img`
   height: 14px;
   width: 14px;
 `;
+
 export const STitle = styled.p`
-  /* font-family: Montserrat; */
   font-weight: 400;
   font-size: 12px;
   line-height: 100%;
-  /* letter-spacing: 0%; */
   text-align: center;
   vertical-align: middle;
-
 `;
--- src/utils/categories.js
+++ src/utils/categories.js
@@ -1,9 +1,17 @@
+import foodIcon from "../assets/icons/food.svg";
+import transportIcon from "../assets/icons/transport.svg";
+import housingIcon from "../assets/icons/housing.svg";
+import joyIcon from "../assets/icons/joy.svg";
+import educationIcon from "../assets/icons/education.svg";
+import othersIcon from "../assets/icons/others.svg";
+
+
 export const categories = [
     { value: "", label: "Все", icon: null },
-    { value: "food", label: "Еда", icon: "" },
-    { value: "transport", label: "Транспорт", icon: "" },
-    { value: "housing", label: "Жилье", icon: "" },
-    { value: "joy", label: "Развлечения", icon: "" },
-    { value: "education", label: "Образование", icon: "" },
-    { value: "others", label: "Другое", icon: "" },
+    { value: "food", label: "Еда", icon: foodIcon },
+    { value: "transport", label: "Транспорт", icon: transportIcon },
+    { value: "housing", label: "Жилье", icon: housingIcon },
+    { value: "joy", label: "Развлечения", icon: joyIcon },
+    { value: "education", label: "Образование", icon: educationIcon },
+    { value: "others", label: "Другое", icon: othersIcon },
 ];
