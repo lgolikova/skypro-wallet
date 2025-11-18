bb7e94a94c57aaf5a029870e6a3fb20b3bd272e2
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-11-18 10:58:02 +0500

    Add key for active icons in objects in array categories

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -4,14 +4,20 @@ import housingIcon from "../assets/icons/housing.svg";
 import joyIcon from "../assets/icons/joy.svg";
 import educationIcon from "../assets/icons/education.svg";
 import othersIcon from "../assets/icons/others.svg";
+import foodIconActive from "../assets/icons/food_active.svg";
+import transportIconActive from "../assets/icons/transport_active.svg";
+import housingIconActive from "../assets/icons/housing_active.svg";
+import joyIconActive from "../assets/icons/joy_active.svg";
+import educationIconActive from "../assets/icons/education_active.svg";
+import othersIconActive from "../assets/icons/others_active.svg";
 
 
 export const categories = [
-    { value: "", label: "Все", icon: null },
-    { value: "food", label: "Еда", icon: foodIcon },
-    { value: "transport", label: "Транспорт", icon: transportIcon },
-    { value: "housing", label: "Жилье", icon: housingIcon },
-    { value: "joy", label: "Развлечения", icon: joyIcon },
-    { value: "education", label: "Образование", icon: educationIcon },
-    { value: "others", label: "Другое", icon: othersIcon },
+    { value: "", label: "Все", icon: null, iconActive: null },
+    { value: "food", label: "Еда", icon: foodIcon, iconActive: foodIconActive },
+    { value: "transport", label: "Транспорт", icon: transportIcon, iconActive: transportIconActive },
+    { value: "housing", label: "Жилье", icon: housingIcon, iconActive: housingIconActive },
+    { value: "joy", label: "Развлечения", icon: joyIcon, iconActive: joyIconActive },
+    { value: "education", label: "Образование", icon: educationIcon, iconActive: educationIconActive },
+    { value: "others", label: "Другое", icon: othersIcon, iconActive: othersIconActive },
 ];
