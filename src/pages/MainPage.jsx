e1c1b7c73fb51a05bb9b5bed826e8957af288adc
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-04-12 13:40:47 +0500

    Remove unused code

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,23 +1,9 @@
 import { Main } from "../components/Main/Main";
 
-const MainPage = ({ transactions, isSpendSelected, onclick }) => {
 
 const MainPage = () => {
     return (
-        <Main
-            transactions={transactions}
-            isSpendSelected={isSpendSelected}
-            onclick={onclick}
-        />
-        <Main
-        />
-        // <>
-        //     <SHeader />
-        //     <SGlobalWrapper>
-        //         <SContainer>Здесь будет код</SContainer>
-        //     </SGlobalWrapper>
-        //     {/* <Outlet /> */}
-        // </>
+        <Main />
     );
 };
 
