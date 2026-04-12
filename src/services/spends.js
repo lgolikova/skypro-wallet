392769aff743a0a34d9df2f677f499a93cd74753
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-04-12 21:35:18 +0500

    Add api function postSpend

--- src/services/spends.js
+++ src/services/spends.js
@@ -20,3 +20,19 @@ export async function fetchSpends(token) {
   }
 };
 
+export async function postSpend(token, newSpend) {
+  try {
+    const resp = await axios.post(API_URL, newSpend, {
+      headers: {
+        "Content-type": "",
+        Authorization: `Bearer ${token}`,
+      }
+    });
+    // console.log("resp.data в апи функции postSpend: ", resp.data);
+    return resp.data;
+  } catch (error) {
+    console.error("Ошибка при добавлении расхода: ", error);
+    throw error;
+  }
+};
+
