d900f2d2c73679e5bf0aa77c26e74602f0325906
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-04-12 13:36:25 +0500

    Add getting spends via api

--- src/context/SpendsProvider.jsx
+++ src/context/SpendsProvider.jsx
@@ -1,20 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { SpendsContext } from "./SpendsContext";
-import { transactions } from "../data";
+// import { transactions } from "../data";
 import { useNavigate } from "react-router-dom";
+import { fetchSpends } from "../services/spends";
+import { AuthContext } from "./AuthContext";
 
 
 export const SpendsProvider = ({ children }) => {
-  const [spends, setSpends] = useState(() => {
-    const savedSpends = localStorage.getItem("spends");
+  // const [spends, setSpends] = useState(() => {
+  //   const savedSpends = localStorage.getItem("spends");
 
-    if (savedSpends) {
-      return JSON.parse(savedSpends);
-    }
+  //   if (savedSpends) {
+  //     return JSON.parse(savedSpends);
+  //   }
 
-    return transactions;
-  });
+  //   return transactions;
+  // });
 
+  const { token } = useContext(AuthContext);
+  // console.log("token в провайдере расходов: ", token);
+
+
+  const [spends, setSpends] = useState([]);
   const [isSpendSelected, setIsSpendSelected] = useState("");
   const [newSpendDescription, setNewSpendDescription] = useState("");
   const [newSpendCategory, setNewSpendCategory] = useState("");
@@ -22,6 +29,26 @@ export const SpendsProvider = ({ children }) => {
   const [newSpendSum, setNewSpendSum] = useState("");
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      getSpends();
+    }
+  }, [token]);
+
+
+  const getSpends = async () => {
+    if (!token) return;
+
+    try {
+      const data = await fetchSpends(token);
+      // console.log("data в провайдере: ", data);
+      setSpends(data);
+    } catch (err) {
+      console.error("Ошибка при получении курсов: ", err);
+    }
+  };
+
 
   const handleSendEditClick = (spendId) => {
     // console.log(`кликнули по строчке с id=${spendId}`);
@@ -69,7 +96,7 @@ export const SpendsProvider = ({ children }) => {
   return (
     <SpendsContext.Provider
       value={{
-        spends,
+        spends, getSpends,
         isSpendSelected, setIsSpendSelected,
         handleSendEditClick,
         addSpend, deleteSpend,
