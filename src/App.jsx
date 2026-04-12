b495cbd3002c540b4ded689c5f890bf6c200666e
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-04-12 13:31:45 +0500

    Add import providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import GlobalStyle from "../src/components/GlobalStyles";
 import AppRoutes from "./AppRoutes";
+import AuthProvider from "./context/AuthProvider";
+import {SpendsProvider} from "./context/SpendsProvider";
+
 
 function App() {
     return (
